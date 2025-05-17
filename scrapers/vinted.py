748e15726d2ce50f7421733614c6510a4e450ef1
psyhorize <bartek.pazdur@gmail.com>
2025-05-17 21:07:43 +0200

    Create vinted.py

---
+++ scrapers/vinted.py
@@ -0,0 +1,10 @@
+def scrape_vinted(query):
+    # Na razie zwracamy dane testowe
+    return {
+        "query": query,
+        "platform": "vinted.de",
+        "lowest_price": 39.99,
+        "highest_price": 89.00,
+        "average_price": 58.25,
+        "offers_count": 27
+    }
